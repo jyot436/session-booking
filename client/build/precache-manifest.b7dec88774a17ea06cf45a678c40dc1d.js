6fd3dbf816efcdbb5ebe7406c896b94e2d1d45e4
jyot436 <jyothi.21bce9552@vitapstudent.ac.in>
2023-11-22 22:42:50 +0530

    final code
    mern app deployment

---
+++ client/build/precache-manifest.b7dec88774a17ea06cf45a678c40dc1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3357e7a386fdd8d0af43b64d96c5bf00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d88d8d78e7947ecf373",
+    "url": "/static/css/2.2a42abf6.chunk.css"
+  },
+  {
+    "revision": "24de41c2692e1218fc54",
+    "url": "/static/css/main.49d19661.chunk.css"
+  },
+  {
+    "revision": "6d88d8d78e7947ecf373",
+    "url": "/static/js/2.41a2c766.chunk.js"
+  },
+  {
+    "revision": "b791f1b247bc2126287e8cb01924979a",
+    "url": "/static/js/2.41a2c766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7943eaafe58df5d4f0e5",
+    "url": "/static/js/3.0e04dba3.chunk.js"
+  },
+  {
+    "revision": "24de41c2692e1218fc54",
+    "url": "/static/js/main.0559a287.chunk.js"
+  },
+  {
+    "revision": "f36776e87f5be377661d",
+    "url": "/static/js/runtime-main.8887d2ef.js"
+  }
+]);
